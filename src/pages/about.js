50eb299cf1afc87da514198ec4cee8c464e6515a
PujaRaj356 <pujarangaraj356@gmail.com>
2025-06-08 17:19:08 +0530

    changed once

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,9 +6,10 @@ export default function About() {
         TrafficPulse is a smart AI-based solution designed to optimize traffic flow in real-time. 
         Our system monitors vehicle patterns and adapts traffic light timings automatically to reduce congestion and delays.
       </p>
+      <br/><hr/>
       <p>
         With live updates and predictive analytics, TrafficPulse helps city traffic authorities manage routes efficiently and sustainably.
-      </p>
+      </p><br/><hr/>
       <p>
         Our goal is to create smoother, safer, and smarter roads using intelligent systems and real-time data.
       </p>
