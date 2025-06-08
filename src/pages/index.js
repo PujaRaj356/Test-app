dbd5c60d5a4e9115b071a6b40ff8ac832e0553a0
PujaRaj356 <pujarangaraj356@gmail.com>
2025-06-08 11:08:15 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+      
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    
+      </footer>
+    </div>
+  );
+}
