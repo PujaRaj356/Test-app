a552c4296aa75eeb45c7b03b869d353eee3cb568
PujaRaj356 <pujarangaraj356@gmail.com>
2025-06-08 19:19:59 +0530

    change1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div>
       <main>
-      <a href="contact.js">CONTACT</a>
+      <link href="contact.js">CONTACT</link>
+      <link href="about.js">ABOUT</link>>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
     
