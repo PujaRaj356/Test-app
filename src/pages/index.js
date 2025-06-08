8d3a23dd387de515da162d9ff4b7352edbd0d85f
PujaRaj356 <pujarangaraj356@gmail.com>
2025-06-08 19:33:57 +0530

    change3

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,7 @@ export default function Home() {
       <link href="contact.js">CONTACT</link>
       <link href="about.js">ABOUT</link>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-    
-      </footer>
+     
     </div>
   );
 }
