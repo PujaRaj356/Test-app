67b35f018b38b803f28dee79b9289080841b8517
PujaRaj356 <pujarangaraj356@gmail.com>
2025-06-08 16:55:26 +0530

    change3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,13 @@
 
 export default function About() {
   return (
-    <div className="about-container">
-      <h2 className="about-heading">About Us</h2>
-      <p className="about-paragraph">
+   <div>
+      <h2>About Us</h2>
+      <p>
         Welcome to our website. We are dedicated to building smart, efficient, and user-friendly
         solutions that help communities and cities grow better.
       </p>
-      <p className="about-paragraph">
+      <p>
         Our goal is to make services accessible, real-time, and easy to use through technology.
       </p>
       <p>
