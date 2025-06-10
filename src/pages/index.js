aea2e97d6ee88a854e0d6513274c74f32ac26899
PujaRaj356 <pujarangaraj356@gmail.com>
2025-06-10 11:21:01 +0530

    changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
             <a className="px-6 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition">Traffic Simulator</a>
           </Link>
           <Link href="/simulator1">
-            <a className="px-6 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition">Congestion Predictor</a>
+            <button>Congestion Predictor</button>
           </Link>
           <Link href="/contact">
             <a className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">Contact</a>
