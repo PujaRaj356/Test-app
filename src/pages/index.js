847c20ef135741a96f6b4df626a235a8ceb58089
PujaRaj356 <pujarangaraj356@gmail.com>
2025-06-10 12:05:41 +0530

    changed the files

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
           <Link href="/simulator">
             <a className="px-6 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition">Traffic Simulator</a>
           </Link>
-         <Link href="/congession">
+         <Link href="/predictor">
   <a className="px-6 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition">
     Congestion Predictor
   </a>
